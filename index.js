d8af35b31fca0b41a0d65894c0b9612e67ff2709
mehedihasan208 <mehedihassan7831@gmail.com>
2023-11-03 17:08:35 +0600

    last commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 require('dotenv').config()
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000 
 
 // middlewar 
-app.use(cors());
+app.use(cors({
+  origin:['http://localhost:5173'],
+  credentials:true
+}));
 app.use(express.json())
+app.use(cookieParser())
 
 const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.BD_PASS}
 @cluster0.ughpgkm.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,12 +32,87 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const serviceCollection = client.db('carDoctor').collection('services')
+    const bookingCollection = client.db('carDoctor').collection('bookings')
+
+    // auth related api 
+    app.post('/jwt',async(req,res)=>{
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+      res
+      .cookie('token',token,{
+        httpOnly:true,
+        secure:true,
+        sameSite:'none'
+      })
+      .send({success:true})
+    })
+
+    //remove cookie
+    app.post('/logout',async(req,res)=>{
+      const user = req.body;
+      console.log('log in out', user)
+      res.clearCookie('token',{maxAge:0}).send({success:true})
+    })
+
+    //services related api 
+    app.get('/services', async(req,res)=>{
+        const cursor = serviceCollection.find();
+        const result = await cursor .toArray();
+        res.send(result)
+    })
+
+    app.get('/services/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId (id)}
+
+        const options = {
+            projection:{title:1, price:1, img:1, service_id:1},
+        };
+
+        const result = await serviceCollection.findOne(query)
+        res.send(result)
+    })
+
+    //bookings
+    app.post('/bookings',async(req,res)=>{
+        const booking =req.body
+        console.log(booking);
+        const result = await bookingCollection.insertOne(booking);
+        res.send(result)
+    })
+    app.get('/bookings',async(req,res)=>{
+        console.log (req.query.email);
+        console.log('cook cookies',req.cookies)
+        let query = {};
+        if(req.query?.email){
+          query = {email:req.query.email}
+        }
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result)
+    })
+    
+    app.delete('bookings/:id', async (req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await bookingCollection.deleteOne(query)
+        res.send(result)
+    })
+
+  //   app.delete('/bookings/:id', async (req, res) => {
+  //     const id = req.params.id;
+  //     const query = { _id: new ObjectId(id) }
+  //     const result = await bookingCollection.deleteOne(query);
+  //     res.send(result);
+  // })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
